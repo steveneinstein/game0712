7da87a10d077b5b5ad21c06638320e34177a205e
ashley <preash63@yahoo.com>
2026-06-05 19:13:31 +0530

    grey other lanes

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lucky-7-cards-v15";
+const CACHE_NAME = "lucky-7-cards-v20";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=15",
-  "./app.js?v=15",
+  "./style.css?v=20",
+  "./app.js?v=20",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
